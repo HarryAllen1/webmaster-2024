d2dc8e00ceee7ed5e3a10d32a8b01fcb7eaaa719
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2024-01-01 22:48:28 -0800

    increase spacing, focus input

--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -1,7 +1,30 @@
 <script lang="ts">
 	import * as Command from '$lib/components/ui/command';
+	import { tick } from 'svelte';
+	import { programs } from '../routes/programs/data';
+
+	const { shouldFocus = false } = $props();
+
+	$effect(() => {
+		if (shouldFocus) {
+			tick().then(() => {
+				(document.querySelector('#search') as HTMLInputElement).focus();
+			});
+		}
+	});
 </script>
 
 <Command.Root class="max-w-[450px] rounded-lg border">
-	<Command.Input placeholder="solar panels" />
+	<Command.Input id="search" placeholder="solar panels" />
+	<Command.List>
+		<Command.Empty>No results found.</Command.Empty>
+		<Command.Group heading="Programs">
+			{#each programs as program}
+				<Command.Item class="flex flex-col items-start">
+					<span class="font-semibold">{program.pageData.title}</span>
+					<span class="text-sm">{program.pageData.description}</span>
+				</Command.Item>
+			{/each}
+		</Command.Group>
+	</Command.List>
 </Command.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,9 +17,9 @@
 		>
 			Search for <span class="text-green-900">green</span> home improvements...
 		</h1>
-		<div class="mt-4 flex flex-row gap-4">
+		<div class="my-32 flex flex-row gap-4">
 			<Button href="/programs" size="lg" class="px-4">Explore Options</Button>
-			<Search />
+			<Search shouldFocus />
 		</div>
 	</div>
 	<div class="flex w-full flex-col">
@@ -31,6 +31,7 @@
 				preserveAspectRatio="none"
 				class="w-full overflow-x-hidden border-b-green-900"
 				version="1.1"
+				style="transition: all 0.4s cubic-bezier(0.16, 1, 0.3, 1) 0s;"
 			>
 				<path
 					d="M 0,51 11.5,43.2 C 23,35.3 46,19.7 69,20.7 c 23,1 46,18.6 69.2,19.8 23.1,1.2 46.5,-14.2 69.6,-21.5 23.2,-7.3 46.2,-6.7 69.2,-0.7 23,6 46,17.4 69,17.4 23,0 46,-11.4 69.2,-18.2 23.1,-6.8 46.5,-9.2 69.6,-3.8 C 508,19 531,32 554,41 c 23,9 46,14 69,16.8 23,2.9 46,3.5 69.2,2.7 23.1,-0.8 46.5,-3.2 69.6,-12.2 C 785,39.3 808,23.7 831,14.5 854,5.3 877,2.7 888.5,1.3 L 900,0 v 178 h -11.5 c -11.5,0 -34.5,0 -57.5,0 -23,0 -46,0 -69.2,0 -23.1,0 -46.5,0 -69.6,0 -23.2,0 -46.2,0 -69.2,0 -23,0 -46,0 -69,0 -23,0 -46,0 -69.2,0 -23.1,0 -46.5,0 -69.6,0 -23.2,0 -46.2,0 -69.2,0 -23,0 -46,0 -69,0 -23,0 -46,0 -69.2,0 -23.1,0 -46.5,0 -69.6,0 -23.2,0 -46.2,0 -69.2,0 -23,0 -46,0 -57.5,0 H 0 Z"
@@ -40,7 +41,7 @@
 				/>
 			</svg>
 		</div>
-		<div class="-mt-8 bg-green-900 pb-8">
+		<div class="-mt-8 bg-green-900 pb-12">
 			<div class="container">
 				<h2
 					class="scroll-m-20 pb-2 font-serif text-5xl font-semibold tracking-tight text-white first:mt-0"
